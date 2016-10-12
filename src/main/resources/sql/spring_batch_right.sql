34484f26ed61deabd0465e2ba05e6caa3f732a56
itmrchen <9700252@qq.com>
2016-10-12 19:26:07 +0800

    去掉主鍵和自增

--- src/main/resources/sql/spring_batch_right.sql
+++ src/main/resources/sql/spring_batch_right.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50018
 File Encoding         : 65001
 
-Date: 2016-10-12 12:33:41
+Date: 2016-10-12 19:24:46
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `user_to`;
 CREATE TABLE `user_to` (
-  `id` bigint(20) NOT NULL auto_increment COMMENT '主键id',
+  `id` bigint(20) NOT NULL COMMENT '主键id',
   `user_name` varchar(64) default '' COMMENT '用户名',
   `sex` int(4) default '0' COMMENT '性别（0：男，1：女）',
   `age` int(4) default '0' COMMENT '年龄',
@@ -28,10 +28,5 @@ CREATE TABLE `user_to` (
   `create_time` datetime default NULL COMMENT '创建时间',
   `update_time` datetime default NULL COMMENT '更新时间',
   `status` int(2) default '1' COMMENT '状态（0：无效，1：有效）',
-  `remark` varchar(64) default '' COMMENT '备注',
-  PRIMARY KEY  (`id`)
+  `remark` varchar(64) default '' COMMENT '备注'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user_to
--- ----------------------------
